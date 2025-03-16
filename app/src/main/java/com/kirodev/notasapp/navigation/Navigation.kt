403eefbbcf69bdcc81db484e4134071f89ae0f60
AngelDZR <98165544+AngelZR17@users.noreply.github.com>
2025-03-15 18:26:33 -0600

    Animacion de navegacion corregida

--- app/src/main/java/com/kirodev/notasapp/navigation/Navigation.kt
+++ app/src/main/java/com/kirodev/notasapp/navigation/Navigation.kt
@@ -1,8 +1,17 @@
 package com.kirodev.notasapp.navigation
 
 import android.content.Context
+import androidx.compose.animation.AnimatedContentTransitionScope
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.scaleIn
+import androidx.compose.animation.scaleOut
+import androidx.compose.animation.slideIn
+import androidx.compose.animation.slideOut
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.unit.IntOffset
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -14,21 +23,26 @@ import com.kirodev.notasapp.ui.screens.TaskScreen
 import com.kirodev.notasapp.util.Preferences
 
 @Composable
-fun Navigation(ctx:Context){
+fun Navigation(ctx:Context) {
     val navController = rememberNavController()
     LaunchedEffect(true) {
-         if(Preferences(ctx).obtenerString("pantalla","").equals("hometasks")) {
+        if (Preferences(ctx).obtenerString("pantalla","").equals("hometasks")) {
             navController.navigate(AppScreens.TasksScreen.route)
-        }   else if(Preferences(ctx).obtenerString("pantalla","").equals("homefavorites")) {
+        } else if (Preferences(ctx).obtenerString("pantalla", "").equals("homefavorites")) {
             navController.navigate(AppScreens.FavoritesScreen.route)
         }
     }
     NavHost(
         navController = navController,
-        startDestination = AppScreens.NotesScreen.route
-    ){
+        startDestination = AppScreens.NotesScreen.route,
+        enterTransition = {
+            fadeIn(animationSpec = tween(durationMillis = 1))
+        },
+        exitTransition = {
+            fadeOut(animationSpec = tween(durationMillis = 1))
+        }) {
         composable(route = AppScreens.NotesScreen.route) {
-            NotesScreen(ctx,navController)
+            NotesScreen(ctx, navController)
         }
         composable(route = AppScreens.TasksScreen.route) {
             TaskScreen(ctx, navController)
