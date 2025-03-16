176de11960d8780c68d154d6b9b94f46209b0652
AngelDZR <98165544+AngelZR17@users.noreply.github.com>
2025-03-16 03:11:32 -0600

    Lazycolumn y card de prueba implementados

--- app/src/main/java/com/kirodev/notasapp/ui/screens/NotesScreen.kt
+++ app/src/main/java/com/kirodev/notasapp/ui/screens/NotesScreen.kt
@@ -2,21 +2,39 @@ package com.kirodev.notasapp.ui.screens
 
 import android.annotation.SuppressLint
 import android.content.Context
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.sizeIn
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Create
+import androidx.compose.material.icons.filled.RestoreFromTrash
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.kirodev.notasapp.navigation.AppScreens
@@ -48,9 +66,51 @@ fun NotesScreen(ctx: Context, navController: NavController) {
         },
         content = { paddingValues ->
             Box(modifier = Modifier.padding(paddingValues)) {
-                Text(modifier = Modifier.fillMaxWidth().padding(horizontal = 20.dp),
-                    text = "Pantalla notas"
-                )
+                LazyColumn(
+                    modifier = Modifier.padding(top = 12.dp)
+                ) {
+                    item {
+                        Card(
+                            modifier = Modifier
+                                .padding(horizontal = 16.dp, vertical = 8.dp)
+                                .clickable {
+                                    println("Diste click prro :v")
+                                }
+                        ) {
+                            Row(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(16.dp)
+                                    .sizeIn(minHeight = 72.dp)
+                            ) {
+                                Column(modifier = Modifier.weight(1f)) {
+                                    Text(
+                                        text = "Titulo nota",
+                                        overflow = TextOverflow.Ellipsis,
+                                        maxLines = 1,
+                                        style = MaterialTheme.typography.displaySmall
+                                    )
+                                    Text(
+                                        text = "Descripcion nota jjsijoisjoijiosjisjoijsoia noots ahi jjjjjj",
+                                        overflow = TextOverflow.Ellipsis,
+                                        maxLines = 1,
+                                        style = MaterialTheme.typography.bodyLarge
+                                    )
+                                    Text(
+                                        text = "16/03/2025",
+                                        style = MaterialTheme.typography.bodyLarge
+                                    )
+                                }
+                                Spacer(Modifier.width(16.dp))
+                                Box(
+                                    modifier = Modifier
+                                        .size(72.dp)
+                                        .clip(RoundedCornerShape(8.dp))
+                                )
+                            }
+                        }
+                    }
+                }
             }
         }
     )
